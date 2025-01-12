a52f94c3c13821bff2e0d4dd3b209b191a7153dd
SalaheddineBensaghir <bensaghir2000@gmail.com>
2025-01-12 12:41:10 +0100

    TP4 v1

---
+++ src/main/java/com/example/tp4devops/Tp4DevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp4devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4DevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp4DevopsApplication.class, args);
+    }
+
+}
